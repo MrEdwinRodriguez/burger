7c9cee83394ade2d937bb011dba4b857a84da92f
Edwin Rodriguez <Edwin Rodriguez>
2016-09-24 18:36:19 -0400

    added config.json

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,13 @@ var connection = mysql.createConnection({
     database: 'burgers_db'
 });
 
+// var connection = mysql.createConnection({
+//     host: '	sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//     user: 'hbf4ejo0g0j9adkj',
+//     password: 'sbz1ll52ytqtsozz',
+//     database: 'burgers_db'
+// });
+
 
 connection.connect(function(err) {
     if (err) {
